248713e9fbdc9f396911a29e8dbf01dadf0e76d9
taynasuperti <taynasuperti@gmail.com>
2025-10-31 21:16:19 -0300

    finalizado o passo 32 do mvc do projeto

--- Pixelix.UI/obj/Debug/net9.0/Pixelix.UI.AssemblyInfo.cs
+++ Pixelix.UI/obj/Debug/net9.0/Pixelix.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pixelix.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be97b2b893e68e8152cca1e5e025e29a00203ea2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caaa0b0b87afed2dafe9e45f75c43490b1d28ad8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pixelix.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pixelix.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
